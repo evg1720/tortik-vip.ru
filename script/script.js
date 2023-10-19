fd54da2d0994f669fcf2755d2573812d203ea3a8
Your Name <evg1720@gmail.com>
2023-10-19 23:16:44 +0800

    media css, captcha.js

--- script/script.js
+++ script/script.js
@@ -1,11 +1,11 @@
 
 new Accordion('.accordion-container')
 
-
+import { MoreList } from "../script/moreList.js"
 
 let clouse_it = "<p>Закрыть</p>"
 
 new MoreList(".list__cakes", 8, clouse_it)
 // второй блок
-new MoreList(".list__cakes__two", 8, clouse_it)
+// new MoreList(".list__cakes__two", 8, clouse_it)
 
