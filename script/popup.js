0028c1bc2e7277600d8d45432b5bd6c505f0c1b4
sergey <oborok05@bk.ru>
2023-11-16 16:14:26 +0300

    zoom and cursor

--- script/popup.js
+++ script/popup.js
@@ -6,6 +6,7 @@ class Popup {
     constructor(popupLink) {
         this.popupLink = popupLink;
         this.popup = document.querySelector(`#${this.popupLink.getAttribute("data-popup-link")}`)
+        this.popupBody = this.popup.querySelector(".popupwrapper__popup")
         this.popupCloseEl = this.popup.querySelector(".popupwrapper__clouse")
 
         const self = this;
@@ -16,17 +17,32 @@ class Popup {
         this.popupCloseEl.addEventListener("click", () => {
             self.popupClose()
         });
+
+        window.addEventListener("resize", () => {
+            self.popupScroll()
+        })
     }
 
     popupOpen() {
         this.popup.classList.remove("none")
+        this.popupScroll()
     }
 
     popupClose() {
         this.popup.classList.add("none")
     }
+
+    popupScroll() {
+        if (this.popupBody.offsetHeight >= (window.innerHeight * 0.88)) {
+            this.popupBody.classList.add("overflow_yscroll")
+        } else {
+            this.popupBody.classList.remove("overflow_yscroll")
+        }
+    }
+
 }
 
 popupLinks.forEach(el => {
     new Popup(el)
 })
+console.log(window.innerHeight);
