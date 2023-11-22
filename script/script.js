50523f4dbe221096e7a8147c67b7aa7cff27a766
Your Name <evg1720@gmail.com>
2023-11-22 11:56:08 +0800

    Fixed: burger, cards, personal account

--- script/script.js
+++ script/script.js
@@ -3,7 +3,7 @@ new Accordion('.accordion-container')
 
 import { MoreList } from "/script/moreList.js"
 
-let clouse_it = "<p>Закрыть</p>"
+let clouse_it = ""
 
 new MoreList(".list__cakes", 8, clouse_it)
 // второй блок
